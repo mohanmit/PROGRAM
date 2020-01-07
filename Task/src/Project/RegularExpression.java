621903d28b734aa2580b8cbe914cd1f19605e97c
hmspl <hmspl@UI-Mahi>
2020-01-07 10:06:13 +0530

    Programs

---
+++ Task/src/Project/RegularExpression.java
@@ -0,0 +1,22 @@
+package Project;
+
+public class RegularExpression {
+
+	public static void main(String[] args) {
+		
+		 String passwd = "Mohan@97"; 
+	      String pattern = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).{8,}";
+	      if  (passwd.matches(pattern))
+	      {
+	    	  System.out.println("valid password");
+	      }
+	      else
+	      {
+	    	  System.out.println(" not a valid password");
+	      }
+	   }
+	
+}
+	
+
+
