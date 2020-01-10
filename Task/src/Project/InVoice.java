9000348114199ab1723310d278f075af63f85ea9
hmspl <hmspl@UI-Mahi>
2020-01-10 11:30:52 +0530

    program

---
+++ Task/src/Project/InVoice.java
@@ -0,0 +1,22 @@
+package Project;
+
+
+public class InVoice {
+
+	public static void main(String[] args) {
+		 String text = "Mobileno: 9840200287  Name: Mohankumar Date: 10/1/2020 Address: chennai,sholinganallur";
+		    System.out.println(getNextWord(text, "Name:"));
+		    System.out.println(getNextWord(text, "Date:"));
+		    System.out.println(getNextWord(text, "Address:"));
+		    System.out.println(getNextWord(text, "Mobileno:"));
+	}
+	public static String getNextWord(String str, String word) {
+	    String[] strArr = str.split(word);
+	    if(strArr.length > 1) {
+	        strArr = strArr[1].trim().split(" ");
+	        return strArr[0];
+	    }
+	    return null;
+	}
+
+}
